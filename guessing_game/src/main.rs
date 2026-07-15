ea5c96424d8cb573336355497b0482e8267b07ce
Avneet Singh Kapoor <avneet.kapoor28@gmail.com>
2026-07-15 17:03:12 +0530

    change guess type from string to int compare guess with secret_number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{cmp::Ordering, io};
 use rand::Rng;
 
 fn main() {
@@ -10,10 +10,16 @@ fn main() {
     println!("Please enter you guess: ");
 
     let mut guess = String::new();
-
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!("You win!!!")
+    }
 }
