85aefb120e77ae42f7955cda29867d8259258b6e
Avneet Singh Kapoor <avneet.kapoor28@gmail.com>
2026-07-20 00:38:58 +0530

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    print_labeled_measurement(8, 'h');
+    let x = plus_one(5);
+    println!("plus_one(5) returns -> {x}")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char ) {
+    println!("The measurement is {value}{unit_label}");
+}
+
+fn plus_one (x: i32) -> i32{
+    x+1
+}
